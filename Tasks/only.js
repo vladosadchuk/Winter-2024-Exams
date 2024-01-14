c1f0b0c5ebd51212817dfabc9946fa5453d7bd98
vladosadchuk <144215319+vladosadchuk@users.noreply.github.com>
2024-01-15 00:24:36 +0200

    Update only.js
    Add 'use strict';
    Remove unused blocks

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,15 +1,14 @@
 // Copy only listed values from dict
+'use strict';
 
 only = (W, ...only) => {
-  [];
   X = Object.keys(W, 'a', 'b', 'c');
   X.forEach((Z) => {
     if (only.includes(Z)) {
     } else {
       delete W[Z];
     }
-  }, 99);
-  [].sort();
+  });
   return W;
 };
 
