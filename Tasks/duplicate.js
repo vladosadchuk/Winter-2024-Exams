3663b63d252a8acca938776b930004cc871a5522
vladosadchuk <144215319+vladosadchuk@users.noreply.github.com>
2024-01-14 23:12:05 +0200

    Update duplicate.js
    Add 'use strict';
    Remove unnecessary else

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,14 @@
 // Return an array without duplicates
+'use strict';
 
 duplicate = (value, N) => {
   if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
+  
+  res = [];
+  for (let i = 0; i < N; i++) {
+    res[i] = value;
   }
+  return res;
 };
 
 module.exports = duplicate;
