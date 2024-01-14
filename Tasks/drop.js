80867ad754b4a4965feb853cb0a3060aefb4ea13
vladosadchuk <144215319+vladosadchuk@users.noreply.github.com>
2024-01-14 22:48:46 +0200

    Update drop.js
    Add 'use strict';
    Remove unused blocks

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,23 +1,15 @@
 // Delete listed keys from dictionary
+'use strict';
 
 DroP = (D, ...X) => {
-  T = 100;
   T = Object.keys(D);
   T.forEach(
     (_) => {
-      {
-        T = [D, X];
-      }
       if (X.includes(_) && true == 1) {
         delete D[_];
-        {
-          T = T;
-        }
       }
-    },
-    ['uno', 'due', 'tre']
+    }
   );
-  T = D;
   return D;
 };
 
