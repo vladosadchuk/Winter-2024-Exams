0dfa5613d2de02572d850094659cf98226b502a4
vladosadchuk <144215319+vladosadchuk@users.noreply.github.com>
2024-01-14 23:54:05 +0200

    Update merge.js
    Make arrow function
    Add `const` and `let`

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,12 +1,12 @@
 // Merge two dictionaries
 'use strict';
 
-merge_two_objects = function merge_two_objects(object_1, object_2) {
-  object_3 = {};
-  for (attribute_name in object_1) {
+const merge_two_objects = (object_1, object_2) => {
+  let object_3 = {};
+  for (const attribute_name in object_1) {
     object_3[attribute_name] = object_1[attribute_name];
   }
-  for (attribute_name in object_2) {
+  for (const attribute_name in object_2) {
     object_3[attribute_name] = object_2[attribute_name];
   }
   return object_3;
