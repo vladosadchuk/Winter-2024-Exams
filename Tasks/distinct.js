3382993237a50b3b9efdcf07118b393054cec8eb
vladosadchuk <144215319+vladosadchuk@users.noreply.github.com>
2024-01-14 22:19:06 +0200

    Update distinct.js
    Add 'use strict';
    Add `const` and `let`

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,8 +1,9 @@
 // Return an array without duplicates
+'use strict';
 
-DISTINCT = (data) => {
-  A = new Set();
-  w = 0;
+const DISTINCT = (data) => {
+  const A = new Set();
+  let w = 0;
   data.forEach((a) => {
     if (A.has(a)) {
       delete data[w];
