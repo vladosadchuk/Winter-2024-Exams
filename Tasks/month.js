25704722a84ffbd66530848216d8e51b1c74b78e
vladosadchuk <144215319+vladosadchuk@users.noreply.github.com>
2024-01-15 00:21:02 +0200

    Update month.js
    Improve names

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,15 +1,15 @@
 // Get month number
 'use strict';
 
-const Month = (s) => {
-  const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
+const getMonth = (strMonth) => {
+  const monthsList = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
                   'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
   
-  for (let i = 0; i < Months.length; i++) {
-    const lowerMonth = s.toLowerCase();
-    if (lowerMonth.indexOf(Months[i]) === 0) return i + 1;
+  for (let i = 0; i < monthsList.length; i++) {
+    const lowerMonth = strMonth.toLowerCase();
+    if (lowerMonth.indexOf(monthsList[i]) === 0) return i + 1;
   }
   return -1;
 };
 
-module.exports = Month;
+module.exports = getMonth;
