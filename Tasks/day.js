40a12ed5fb8e0ecbdefbd7213f352b2f69894a83
vladosadchuk <144215319+vladosadchuk@users.noreply.github.com>
2024-01-14 21:49:34 +0200

    Update day.js
    Replace for with for...of
    Create variable lowerDay

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,9 +4,10 @@
 const parseDay = (strDay) => {
   const daysList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   
-  for (let i = 0; i < daysList.length; i++) {
-    if (strDay.startsWith(daysList[i].toLowerCase())) {
-      return i + 1;
+  for (const [index, day] of daysList.entries()) {
+    const lowerDay = day.toLowerCase();
+    if (strDay.startsWith(lowerDay)) {
+      return index + 1;
     }
   }
   return -1;
