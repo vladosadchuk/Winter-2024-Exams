4073d256ebfeaec9d4e3b098e7513a23adfee0c1
vladosadchuk <144215319+vladosadchuk@users.noreply.github.com>
2024-01-15 00:14:46 +0200

    Update month.js
    Add `const`
    Remove variable l

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,12 +1,11 @@
 // Get month number
 'use strict';
 
-Month = (s) => {
-  Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
-            'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+const Month = (s) => {
+  const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
+                  'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
   
-  l = Months.length;
-  for (let i = 0; i < l; i++) {
+  for (let i = 0; i < Months.length; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
   return -1;
